10bd78908601238253a0e2ed1cdf6ce0b2f40cc9
38068 <38068@DESKTOP-SRPFKQ2>
2020-10-18 17:57:24 +0300

    first commit

--- queries.js
+++ queries.js
@@ -1,12 +1,16 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    host: 'ec2-54-217-206-236.eu-west-1.compute.amazonaws.com',
-    database: 'd8pg59uki17vj',
-    user: 'utotqzeajjkolm',
-    password: '2fea7ccdb7fb8a361d60bc97272d6e68e01ed655425c930bbbc3eed760436fda',
-    port: '5432',
-    ssl: true
+    /*host: 'ec2-54-217-206-236.eu-west-1.compute.amazonaws.com',
+     database: 'd8pg59uki17vj',
+     user: 'utotqzeajjkolm',
+     password: '2fea7ccdb7fb8a361d60bc97272d6e68e01ed655425c930bbbc3eed760436fda',
+     port: '5432',*/
+    connectionString: 'postgres://utotqzeajjkolm:2fea7ccdb7fb8a361d60bc97272d6e68e01ed655425c930bbbc3eed760436fda@ec2-54-217-206-236.eu-west-1.compute.amazonaws.com:5432/d8pg59uki17vj',
+    ssl: { rejectUnauthorized: false }
 })
+
+
+
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM work1.table_sample', (error, results) => {
         if (error) {
