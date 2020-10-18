196dbeb5eb34d02a89f5439042d8a433fb5eb2e3
38068 <38068@DESKTOP-SRPFKQ2>
2020-10-18 16:58:56 +0300

    first commit

--- queries.js
+++ queries.js
@@ -1,29 +1,34 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
- host: 'localhost',
-  database: 'housedb',
-  user:'mit21',
-  password: 'mit21',
-  port: 5432,
+    host: 'ec2-54-217-206-236.eu-west-1.compute.amazonaws.com',
+    database: 'd8pg59uki17vj',
+    user: 'utotqzeajjkolm',
+    password: '2fea7ccdb7fb8a361d60bc97272d6e68e01ed655425c930bbbc3eed760436fda',
+    port: '5432',
+    ssl: true,
+    dialect: 'postgres',
+    dialectOptions: {
+        "ssl": {"require": true}
+    }
 })
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM table_sample ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
+    pool.query('SELECT * FROM table_sample ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
 }
 
 const getUserById = (request, response) => {
-  const id = parseInt(request.params.id)
+    const id = parseInt(request.params.id)
 
-  pool.query('SELECT * FROM table_sample WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
+    pool.query('SELECT * FROM table_sample WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
 }
 
 
@@ -33,6 +38,6 @@ const getUserById = (request, response) => {
 
 
 module.exports = {
-  getUsers,
-  getUserById  
+    getUsers,
+    getUserById
 }
